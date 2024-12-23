7c040c8463409215722a0efe96896477270a0fff
Naveen <notnaveen2@gmail.com>
2024-12-23 13:38:46 +0530

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.get("/", function (req, res) {
   res.send("home");
@@ -15,6 +23,5 @@ app.post("/post", function (req, res) {
   res.json({ msg: "Post method" });
 });
 
-app.use(express.json());
 app.listen(3000);
 module.exports = app;
--- sampleFrontend/src/App.jsx
+++ sampleFrontend/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import axios from "axios";
+const App = () => {
+  const handleClick = async () => {
+    const respose = await axios
+      .post("https://sample-backend-sigma.vercel.app/post", {
+        message: "test",
+      })
+      .then((response) => {
+        console.log(response.data.msg);
+      });
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button onClick={handleClick}>post</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
